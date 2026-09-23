29d31da09ffa62cb87bca4e3bf70735e470b6663
briangavilanes <briang06810@gmail.com>
2026-09-23 13:29:45 -0400

    Added variables asking for the user input

--- Program2_PrintVowelsAndNonVowels.java
+++ Program2_PrintVowelsAndNonVowels.java
@@ -1,4 +1,12 @@
+import java.util.Scanner;
+
 public class Program2_PrintVowelsAndNonVowels {
   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("| Please input a string:");
+    String userString = input.nextLine();
+
+    input.close();
   }
 }
