8186d5accb032af467ce28fb477432abd7af74d8
briangavilanes <briang06810@gmail.com>
2026-09-23 14:24:15 -0400

    Added a while true statement checking each character for any vowels

--- Program2_PrintVowelsAndNonVowels.java
+++ Program2_PrintVowelsAndNonVowels.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Program2_PrintVowelsAndNonVowels {
@@ -6,6 +7,23 @@ public class Program2_PrintVowelsAndNonVowels {
 
     System.out.println("| Please input a string:");
     String userString = input.nextLine();
+
+    int textLength = userString.length();
+
+    int nonVowelCount = 0;
+    int vowelCount = 0;
+
+    while (textLength > 0) {
+      textLength -= 1;
+
+      char letter = userString.charAt(textLength);
+
+      if (letter == 'A' || letter == 'a' || letter == 'E' || letter == 'e' || letter == 'I' || letter == 'i' || letter == 'O' || letter == 'o' || letter == 'U' || letter == 'u') {
+        nonVowelCount += 1;
+      } else {
+        vowelCount += 1;
+      }
+    }
 
     input.close();
   }
