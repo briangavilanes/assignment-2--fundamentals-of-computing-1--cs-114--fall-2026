7ca6ea137ca4c816433708068110338caa654a23
briangavilanes <briang06810@gmail.com>
2026-09-23 15:13:00 -0400

    Added a switch statement declaring the abbreviation from day number

--- Program3_TheTwelveDaysOfChristmas.java
+++ Program3_TheTwelveDaysOfChristmas.java
@@ -1,4 +1,22 @@
 public class Program3_TheTwelveDaysOfChristmas {
   public static void main(String[] args) {
+    for (int i = 1; i <= 12; i++) {
+      String message = "On the " + i;
+
+      switch (i) {
+        case 1:
+          message += "st";
+          break;
+        case 2:
+          message += "nd";
+          break;
+        case 3:
+          message += "rd";
+          break;
+        default:
+          message += "th";
+          break;
+      }
+    }
   }
 }
