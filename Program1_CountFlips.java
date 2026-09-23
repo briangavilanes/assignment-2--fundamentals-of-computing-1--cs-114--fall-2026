84d812f1e452812c52546dda221ee7829eff19bd
briangavilanes <briang06810@gmail.com>
2026-09-23 12:10:33 -0400

    Added variables to start off

--- Program1_CountFlips.java
+++ Program1_CountFlips.java
@@ -1,4 +1,8 @@
 public class Program1_CountFlips {
   public static void main(String[] args) {
+    final int finalRound = 100;
+    int roundCounter = 0;
+    int headsCount = 0;
+    int tailsCount = 0;
   }
 }
