73f6ad24e19c99a2d83b8763ce04ced9c17e634a
briangavilanes <briang06810@gmail.com>
2026-09-23 12:11:58 -0400

    Created while loop statement until it reaches the condition

--- Program1_CountFlips.java
+++ Program1_CountFlips.java
@@ -4,5 +4,19 @@ public class Program1_CountFlips {
     int roundCounter = 0;
     int headsCount = 0;
     int tailsCount = 0;
+
+    while (roundCounter != finalRound) {
+      roundCounter += 1;
+
+      Coin coin = new Coin();
+
+      if (coin.isHeads() == true) {
+        headsCount += 1;
+      } else {
+        tailsCount += 1;
+      }
+    }
+
+    System.out.println("| Results |\n| Heads: " + headsCount + "\n| Tails: " + tailsCount );
   }
 }
